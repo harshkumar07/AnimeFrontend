30abbda771622301d8e9572a490bb6fef78f7c46
harshkumar07 <harshku068@gmail.com>
2024-11-03 23:59:05 +0530

    Added Next Config File

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Enables React Strict Mode
+    // Add any other Next.js configuration options here
+  };
+  
+  module.exports = nextConfig;
+  
