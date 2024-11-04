8a36e625daaa22b912bd1eb2c06a3cd09694f208
harshkumar07 <harshku068@gmail.com>
2024-11-04 22:04:50 +0530

    Trending Page Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-// app/page.tsx (or pages/index.tsx if you're not using the app directory)
 import TopAiring from "@/Pages/TopAiring";
 import React from "react";
 
-
 const HomePage: React.FC = () => {
   return (
-    <main className="w-full">
-      
-    <TopAiring/>
+    <main className="w-full h-full">
+      <TopAiring /> {/* Ensure TopAiring handles the className prop */}
     </main>
   );
 };
